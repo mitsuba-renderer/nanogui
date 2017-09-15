e6db41c3ecf7d15ab5d76adf85b5e42bb040b47d
Wenzel Jakob <wenzel.jakob@epfl.ch>
2017-09-15 17:07:10 +0200

    quench warnings on GCC 7

--- src/common.cpp
+++ src/common.cpp
@@ -133,6 +133,14 @@ void shutdown() {
     glfwTerminate();
 }
 
+#if defined(__clang__)
+#  define NANOGUI_FALLTHROUGH [[clang::fallthrough]];
+#elif defined(__GNUG__)
+#  define NANOGUI_FALLTHROUGH __attribute__ ((fallthrough));
+#else
+#  define NANOGUI_FALLTHROUGH
+#endif
+
 std::array<char, 8> utf8(int c) {
     std::array<char, 8> seq;
     int n = 0;
@@ -144,11 +152,11 @@ std::array<char, 8> utf8(int c) {
     else if (c <= 0x7fffffff) n = 6;
     seq[n] = '\0';
     switch (n) {
-        case 6: seq[5] = 0x80 | (c & 0x3f); c = c >> 6; c |= 0x4000000;
-        case 5: seq[4] = 0x80 | (c & 0x3f); c = c >> 6; c |= 0x200000;
-        case 4: seq[3] = 0x80 | (c & 0x3f); c = c >> 6; c |= 0x10000;
-        case 3: seq[2] = 0x80 | (c & 0x3f); c = c >> 6; c |= 0x800;
-        case 2: seq[1] = 0x80 | (c & 0x3f); c = c >> 6; c |= 0xc0;
+        case 6: seq[5] = 0x80 | (c & 0x3f); c = c >> 6; c |= 0x4000000; NANOGUI_FALLTHROUGH
+        case 5: seq[4] = 0x80 | (c & 0x3f); c = c >> 6; c |= 0x200000;  NANOGUI_FALLTHROUGH
+        case 4: seq[3] = 0x80 | (c & 0x3f); c = c >> 6; c |= 0x10000;   NANOGUI_FALLTHROUGH
+        case 3: seq[2] = 0x80 | (c & 0x3f); c = c >> 6; c |= 0x800;     NANOGUI_FALLTHROUGH
+        case 2: seq[1] = 0x80 | (c & 0x3f); c = c >> 6; c |= 0xc0;      NANOGUI_FALLTHROUGH
         case 1: seq[0] = c;
     }
     return seq;
