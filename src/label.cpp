e44f5f5d4865d6fa5b2b05c7feeff063a21483e0
Thomas Müller <git@tom94.net>
2026-06-23 10:44:41 +0200

    fix(label): correct sizing and rendering of non-fixed multiline strings

--- src/label.cpp
+++ src/label.cpp
@@ -41,12 +41,17 @@ Vector2i Label::preferred_size_impl(NVGcontext *ctx) const {
     if (m_fixed_size.x() > 0) {
         float bounds[4];
         nvgTextAlign(ctx, NVG_ALIGN_LEFT | NVG_ALIGN_TOP);
-        nvgTextBoxBounds(ctx, m_pos.x(), m_pos.y(), m_fixed_size.x(), m_caption.c_str(), nullptr, bounds);
+        nvgTextBoxBounds(ctx, m_pos.x(), m_pos.y(), m_fixed_size.x(), m_caption.data(), m_caption.data() + m_caption.size(), bounds);
         return Vector2i(m_fixed_size.x(), bounds[3] - bounds[1]);
+    } else if (m_caption.find('\n') != std::string::npos) { // multiline
+        float bounds[4];
+        nvgTextAlign(ctx, NVG_ALIGN_LEFT | NVG_ALIGN_TOP);
+        nvgTextBoxBounds(ctx, 0, 0, 1e10f, m_caption.data(), m_caption.data() + m_caption.size(), bounds);
+        return Vector2i(bounds[2] - bounds[0], bounds[3] - bounds[1]);
     } else {
         nvgTextAlign(ctx, NVG_ALIGN_LEFT | NVG_ALIGN_MIDDLE);
         return Vector2i(
-            nvgTextBounds(ctx, 0, 0, m_caption.c_str(), nullptr, nullptr) + 2,
+            nvgTextBounds(ctx, 0, 0, m_caption.data(), m_caption.data() + m_caption.size(), nullptr) + 2,
             font_size()
         );
     }
@@ -59,10 +64,13 @@ void Label::draw(NVGcontext *ctx) {
     nvgFillColor(ctx, m_color);
     if (m_fixed_size.x() > 0) {
         nvgTextAlign(ctx, NVG_ALIGN_LEFT | NVG_ALIGN_TOP);
-        nvgTextBox(ctx, m_pos.x(), m_pos.y(), m_fixed_size.x(), m_caption.c_str(), nullptr);
+        nvgTextBox(ctx, m_pos.x(), m_pos.y(), m_fixed_size.x(), m_caption.data(), m_caption.data() + m_caption.size());
+    } else if (m_caption.find('\n') != std::string::npos) { // multiline
+        nvgTextAlign(ctx, NVG_ALIGN_LEFT | NVG_ALIGN_TOP);
+        nvgTextBox(ctx, m_pos.x(), m_pos.y(), preferred_size(ctx).x(), m_caption.data(), m_caption.data() + m_caption.size());
     } else {
         nvgTextAlign(ctx, NVG_ALIGN_LEFT | NVG_ALIGN_MIDDLE);
-        nvgText(ctx, m_pos.x(), m_pos.y() + m_size.y() * 0.5f, m_caption.c_str(), nullptr);
+        nvgText(ctx, m_pos.x(), m_pos.y() + m_size.y() * 0.5f, m_caption.data(), m_caption.data() + m_caption.size());
     }
 }
 
